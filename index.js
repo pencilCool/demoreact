1c634e4cf2d541803dcf968ed8c30b24cd1d9ded
pencilCool <yhtangcoder@gmail.com>
2023-04-02 10:49:30 +0800

    refactor: track and trigger in proxy

--- index.js
+++ index.js
@@ -5,28 +5,34 @@ const bucket = new WeakMap()
 const  data = {text:"hello world"}
 const obj = new Proxy(data,{
   get(target,key) {
-    if(!activeEffect) return target[key]
-    let depsMap = bucket.get(target) 
-    if (!depsMap) {
-      bucket.set(target,(depsMap = new Map()))
-    }
-    let deps = depsMap.get(key)
-    if(!deps) {
-      depsMap.set(key,(deps = new Set()))    
-    }
-    deps.add(activeEffect)
+    track(target,key)
     return target[key]
   },
   set(target,key,newVal) {
     target[key] = newVal
-    
-    const depsMap = bucket.get(target)
-    if(!depsMap) return
-    const effects = depsMap.get(key)
-    effects && effects.forEach(fn => fn());
+    trigger(target,key)
   }
 })
 
+function track(target,key) {
+  if(!activeEffect) return target[key]
+  let depsMap = bucket.get(target) 
+  if (!depsMap) {
+    bucket.set(target,(depsMap = new Map()))
+  }
+  let deps = depsMap.get(key)
+  if(!deps) {
+    depsMap.set(key,(deps = new Set()))    
+  }
+  deps.add(activeEffect)
+}
+
+function trigger(target,key) {
+  const depsMap = bucket.get(target)
+  if(!depsMap) return
+  const effects = depsMap.get(key)
+  effects && effects.forEach(fn => fn());
+}
 function  effect(fn) {
   activeEffect = fn
   fn() 
@@ -40,4 +46,9 @@ effect(()=>{
 setTimeout(()=>{
   obj.noExist = "hello vue3"
 },1000)
+
+setTimeout(()=>{
+  obj.text = "hello you"
+},1000)
+
 
